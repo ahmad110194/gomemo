129230afabf707f90f5159d56e701f2b67312936
Ahmad Hafizh <ahmad.hafizh@stockbit.com>
2024-07-22 17:48:13 +0700

    first init case 1 - new relic context

---
+++ case1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	_ "net/http/pprof"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+func main() {
+	newrelicApp, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("local-testing-case-1"),
+		newrelic.ConfigLicense("__YOUR_NEW_RELIC_LICENSE_KEY__1234567890"),
+	)
+	if err != nil {
+		fmt.Println("Error initializing New Relic:", err)
+		return
+	}
+
+	app := &App{newrelicApp: newrelicApp}
+
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	http.HandleFunc("/", app.handler)
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+type App struct {
+	newrelicApp *newrelic.Application
+}
+
+func (a *App) handler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithCancel(r.Context())
+	defer cancel()
+
+	newrelicTx := a.newrelicApp.StartTransaction("transaction_name")
+	defer newrelicTx.End()
+
+	ctxWithNewRelicTransaction := newrelic.NewContext(ctx, newrelicTx)
+
+	go processRequest(ctxWithNewRelicTransaction)
+
+	// Simulate a delay to keep the handler active longer
+	time.Sleep(10 * time.Second)
+}
+
+func processRequest(ctx context.Context) {
+	select {
+	case <-time.After(2 * time.Second):
+		fmt.Println("Request processed")
+	case <-ctx.Done():
+		fmt.Println("Request canceled")
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
-package gomemo
+package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello")
+	iterations := flag.Int("i", 1, "Number of iterations to make HTTP requests")
+	flag.Parse()
+
+	if *iterations < 1 {
+		fmt.Println("Iterations must be greater than 0")
+		os.Exit(1)
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < *iterations; i++ {
+		wg.Add(1)
+
+		delay := time.Duration(rand.Intn(300)) * time.Millisecond
+		time.Sleep(delay)
+		go makeRequest(i, &wg)
+	}
+
+	wg.Wait()
+}
+
+func makeRequest(index int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	resp, err := http.Get("http://localhost:8080")
+	if err != nil {
+		fmt.Printf("Error making request %d: %v\n", index+1, err)
+		return
+	}
+	defer resp.Body.Close()
 }
